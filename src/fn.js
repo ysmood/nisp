72abdcd9c5434e4c98e73e21d614083cd6b011bc
Yad Smood <y.s.inside@gmail.com>
2015-12-04 16:50:55 +0800

    upd: doc

--- src/fn.js
+++ src/fn.js
@@ -7,14 +7,17 @@ function assign (obj, src) {
 
 module.exports = function (fnAst) {
     return function (ast, env, eval) {
+        // generate a closure
         var closure = assign({}, env);
         var i, len, ret;
 
+        // assign arguments to closure
         len = fnAst[1].length;
         for (i = 0; i < len; i++) {
             closure[eval(fnAst[1][i], env)] = eval(ast[i + 1], env);
         }
 
+        // eval the function
         len = fnAst.length;
         for (i = 2; i < fnAst.length; i++) {
             ret = eval(fnAst[i], closure);
