65fef791c539f4fe102b6f4e060655405c436d4b
Yad Smood <y.s.inside@gmail.com>
2016-04-16 12:28:06 +0800

    v0.5.5

---
+++ fn/plainSpread.js
@@ -0,0 +1,12 @@
+// ["fn", <arg1>, <arg2>, ...]
+module.exports = function (fn) {
+    return function (run, args, sandbox, env) {
+        var plainArgs = [], len = args.length;
+
+        for (var i = 1; i < len; i++) {
+            plainArgs[i - 1] = run(args[i], sandbox, env);
+        }
+
+        return fn.apply(env, plainArgs);
+    };
+};
