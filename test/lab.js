57c17dd948bce665e121a891f131845de71ed52e
muxrwc <muxrwc@126.com>
2017-01-10 18:01:39 +0800

    add: nisp grammar

--- test/lab.js
+++ test/lab.js
@@ -1,17 +1,36 @@
-/*eslint-disable */
+var nisp = require("../core");
+var Promise = require("yaku");
+var yutils = require("yaku/lib/utils");
 
-var nisp = require('../core');
 
-
-var sandbox = {
-    "def": require('../lang/def'),
-    "": require('../lang/plain'),
-    "fn": require('../lang/fn'),
-    "$": require('../lang/get'),
-    "do": require('../lang/do'),
-    a: 100
+var fns = {
+    plain: require("../fn/plain"),
+    plainSpread: require("../fn/plainSpread"),
+    plainAsync: require("../fn/plainAsync"),
+    plainAsyncSpread: require("../fn/plainAsyncSpread"),
+    args: require("../fn/args")
 };
 
-var expresses = ["b", 1]
 
-console.log(nisp(expresses, sandbox)); // => 20
+var langs = {
+    do: require("../lang/do"),
+    if: require("../lang/if"),
+    plain: require("../lang/plain"),
+    def: require("../lang/def"),
+    fn: require("../lang/fn"),
+    list: require("../lang/list"),
+    dict: require("../lang/dict"),
+
+    add: fns.plain(function (args) {
+        return args.reduce(function (s, v) {
+            return s += v;
+        });
+    })
+};
+
+var tpl = nisp.new`
+    ( add 1 2 3 ( add 1 2 ) 5 ${6} )
+`;
+
+
+console.log(tpl(langs))
