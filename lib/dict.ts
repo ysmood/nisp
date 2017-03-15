4b173546f9efe9bdc595723a3df7656829e4bf2d
jcl <jcl@jcldeMacBook-Pro.local>
2017-03-15 16:23:01 +0800

    edit the dict odd bug

--- lib/dict.ts
+++ lib/dict.ts
@@ -2,10 +2,12 @@
 export default function () {
     var dict = {};
     var args = arguments
-
+    if(args.length % 2 == 1){
+        throw new Error(`odd dict error`);
+    }
     for (var i = 0, j; i < args.length; i = ++j) {
         j = i + 1;
-        dict[args[i]] = args[j];
+        dict[args[i]] = args[j] || null;
     }
 
     return dict;
