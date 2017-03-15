dc6c20e72f2277f020874cd6f7193d7db71b841d
Yad Smood <ysmood@users.noreply.github.com>
2017-03-15 19:04:34 +0800

    Update dict.ts

--- lib/dict.ts
+++ lib/dict.ts
@@ -1,10 +1,10 @@
-// ["list", k1, v1, k2, v2, ...]
+// ["dict", k1, v1, k2, v2, ...]
 import { error } from '../core/index'
 export default function () {
     var dict = {};
     var args = arguments
     if(args.length % 2 == 1){
-        return error(this,`odd dict error`);
+        return error(this, `the amount keys and values should be same`);
     }
     for (var i = 0, j; i < args.length; i = ++j) {
         j = i + 1;
