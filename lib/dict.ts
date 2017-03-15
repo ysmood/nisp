0355f4ae56b74649a2ea6a4f0f813a927b4e9da3
jcl <jcl@jcldeMacBook-Pro.local>
2017-03-15 18:29:47 +0800

    修改error,null改成undefined

--- lib/dict.ts
+++ lib/dict.ts
@@ -1,13 +1,14 @@
 // ["list", k1, v1, k2, v2, ...]
+import { error } from '../core/index'
 export default function () {
     var dict = {};
     var args = arguments
     if(args.length % 2 == 1){
-        throw new Error(`odd dict error`);
+        return error(this,`odd dict error`);
     }
     for (var i = 0, j; i < args.length; i = ++j) {
         j = i + 1;
-        dict[args[i]] = args[j] || null;
+        dict[args[i]] = args[j] || undefined;
     }
 
     return dict;
