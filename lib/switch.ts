d2b71deafbaed881464d61d35e979ad0e9fa1b24
信鑫-King <45808948@qq.com>
2017-04-19 17:12:20 +0800

    upd: switch-case function

--- lib/switch.ts
+++ lib/switch.ts
@@ -1,14 +1,15 @@
 import nisp, { error, macro } from '../core'
 
 export default macro((ctx) => {
-    const { sandbox, ast, env, index } = ctx;
-    ast.shift()
+    let { sandbox, ast, env, index } = ctx
+    ast = ast.slice(1)
     if (!ast.length) {
         return
     }
     let defaultNode
     if (ast[ast.length - 1][0] === 'default') {
-        defaultNode = ast.pop()
+        defaultNode = ast[ast.length - 1]
+        ast = ast.slice(0, -1)
     }
     if (!ast.length) {
         return nisp(defaultNode[1], sandbox, env, ctx, index)
@@ -16,8 +17,9 @@ export default macro((ctx) => {
     let expressionValue
     let hasExpression = false
     if (ast[0][0] !== 'case') {
-        expressionValue = nisp(ast.shift(), sandbox, env, ctx, index)
+        expressionValue = nisp(ast[0], sandbox, env, ctx, index)
         hasExpression = true
+        ast = ast.slice(1)
     }
     for (let node of ast) {
         if (node[0] !== 'case') {
